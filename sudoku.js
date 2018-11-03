9b41e31a0b4b9e3402c5046e8d50c8d03fe8b427
abednegolubis <lubisabednego@gmail.com>
2018-11-03 13:31:40 +0700

    submit challenges  sudoku

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,90 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.sudokuBoard = board_string,
+    this.arrBoard = []
+    this.kosong = []
+  }
 
-  solve() {}
+  checkHorizontal(row,numberInput) {
+    for(let i = row; i < this.arrBoard.length; i++){
+        if(this.arrBoard[row][i] == numberInput) {
+          return  false
+        }
+    }
+    return true
+  }
+  checkVertical(col,numberInput ) {
+    for(let i = 0; i < this.arrBoard.length; i++){
+      if(this.arrBoard[i][col] == numberInput) {
+        return  false
+      }
+    }
+    return true
+  }
+  checkBox(row,col,numberInput) {
+    var xStart = 3 *(Math.floor(row/3))
+    var xEnd = xStart + 2
+    var yStart = 3 *(Math.floor(col/3))
+    var yEnd = yStart + 2
 
+    for(let i = xStart; i <= xEnd; i++) {
+      for(let j = yStart; j <= yEnd; j++){
+        if(this.arrBoard[i][j] == numberInput){
+          return false
+        }
+      }
+    }
+    return true
+  }
+
+  checkKosong() {
+    for(let i = 0; i < this.arrBoard.length; i++){
+      for(let j = 0; j < this.arrBoard.length; j++) {
+        if(this.arrBoard[i][j] === '0') {
+          let obj =  {
+            x : i,
+            y : j,
+            value : this.arrBoard[i][j]
+          }
+        }
+      }
+    }
+  }
+
+  solve() {
+    // console.log(this.arrBoard)
+    for(let i = 0; i < this.arrBoard.length; i++) {
+      for(let j = 0; j < this.arrBoard[i].length; j++) {
+        if(this.arrBoard[i][j] === '0') {
+          let status = false
+          var k = 1
+          while(k <= 9 && status === false) {
+            let checkRow = this.checkHorizontal(i,k) 
+            let checkCol =  this.checkVertical(j,k)
+            let checkGrid = this.checkBox(i,j,k)
+            if(checkRow === true && checkCol === true && checkGrid === true) {
+              this.arrBoard[i][j] = String(k)
+              status = true
+            } else {
+              k++
+            }
+          }
+        }
+      }
+    }
+  }
+  createBoard() {
+    for (let i = 0; i < this.sudokuBoard.length-1; i+=9) {
+      var baris = this.sudokuBoard.slice(i,i+9).split('')
+      this.arrBoard.push(baris)
+    }  
+  }
   // Returns a string representing the current state of the board
-  board() {}
+  printBoard() {
+    return this.arrBoard                                                                                           
+  }
 }
 
 // The file has newlines at the end of each line,
@@ -19,6 +97,7 @@ var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
 var game = new Sudoku(board_string)
 
 // Remember: this will just fill out what it can and not "guess"
+game.createBoard()
 game.solve()
 
-console.log(game.board())
+console.log(game.printBoard())
---
+++ test.js
@@ -0,0 +1,4 @@
+var myString = 'HelloWorld. How are you doing?';
+var splits = myString.split(' ', 3);
+
+console.log(splits);
